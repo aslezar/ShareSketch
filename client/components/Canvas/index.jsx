dd15cfdc56074c97dbaa87070cebc8250d3ed7fb
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-08-10 15:47:18 +0530

    canvas ready, backend initiated

--- client/components/Canvas/index.jsx
+++ client/components/Canvas/index.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+// FabricCanvas.js (or any other component)
 
-const Canvas = () => {
-	return <div>Canvas</div>;
+import React, { useEffect, useRef } from 'react';
+import { fabric } from './fabric.js';
+
+const FabricCanvas = () => {
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const canvas = new fabric.Canvas(canvasRef.current);
+
+		// Add your Fabric.js code here
+		// For example, you can create and add shapes to the canvas
+		const rect = new fabric.Rect({
+			width: 100,
+			height: 100,
+			fill: 'red',
+			left: 50,
+			top: 50,
+		});
+		canvas.add(rect);
+
+		// Clean up Fabric.js objects when component unmounts
+		return () => {
+			canvas.dispose();
+		};
+	}, []);
+
+	return <canvas ref={canvasRef} />;
 };
 
-export default Canvas;
+export default FabricCanvas;
