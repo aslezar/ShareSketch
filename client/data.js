31c25b24269c0d9c3ce97300ab1c1720bfdfd744
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-09-03 13:52:47 +0530

    added socials

--- client/data.js
+++ client/data.js
@@ -1,12 +1,12 @@
-const twitter = 'https://twitter.com';
-const facebook = 'https://facebook.com';
-const instagram = 'https://instagram.com';
-const linkedin = 'https://linkedin.com';
-const github = 'https://github.com';
+const twitter = 'https://twitter.com/mr_shivamgarg';
+// const facebook = 'https://facebook.com';
+// const instagram = 'https://instagram.com';
+const linkedin = 'https://www.linkedin.com/in/shivamgarg1234/';
+const github = 'https://github.com/aslezar';
 const socials = {
 	twitter,
-	facebook,
-	instagram,
+	// facebook,
+	// instagram,
 	linkedin,
 	github,
 };
